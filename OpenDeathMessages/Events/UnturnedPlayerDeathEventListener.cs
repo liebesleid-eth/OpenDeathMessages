10e695a069cd7bf1dc1d6914e07b549895f16049
liebesleid-eth <99780369+liebesleid-eth@users.noreply.github.com>
2023-01-31 00:14:27 -0800

    Update UnturnedPlayerDeathEventListener.cs

--- OpenDeathMessages/Events/UnturnedPlayerDeathEventListener.cs
+++ OpenDeathMessages/Events/UnturnedPlayerDeathEventListener.cs
@@ -1,7 +1,8 @@
-﻿using EvolutionPlugins.OpenDeathMessages.API;
+using EvolutionPlugins.OpenDeathMessages.API;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Localization;
 using OpenMod.API.Eventing;
+using OpenMod.API.Permissions;
 using OpenMod.Unturned.Locations;
 using OpenMod.Unturned.Players.Life.Events;
 using OpenMod.Unturned.Users;
@@ -18,22 +19,29 @@ namespace EvolutionPlugins.OpenDeathMessages.Events
         private readonly IConfiguration m_Configuration;
         private readonly IUnturnedLocationDirectory m_UnturnedLocationDirectory;
         private readonly IPlayerMessager m_PlayerMessager;
+        private readonly IPermissionChecker m_PermissionChecker;
 
         public UnturnedPlayerDeathEventListener(IUnturnedUserDirectory unturnedUserDirectory,
             IStringLocalizer stringLocalizer, IConfiguration configuration,
-            IUnturnedLocationDirectory unturnedLocationDirectory, IPlayerMessager playerMessager)
+            IUnturnedLocationDirectory unturnedLocationDirectory, IPlayerMessager playerMessager, IPermissionChecker permissionChecker)
         {
             m_UnturnedUserDirectory = unturnedUserDirectory;
             m_StringLocalizer = stringLocalizer;
             m_Configuration = configuration;
             m_UnturnedLocationDirectory = unturnedLocationDirectory;
             m_PlayerMessager = playerMessager;
+            m_PermissionChecker = permissionChecker;
         }
 
         public async Task HandleEventAsync(object? sender, UnturnedPlayerDeathEvent @event)
         {
             var victimUser = m_UnturnedUserDirectory.FindUser(@event.Player.SteamId);
             if (victimUser == null)
+            {
+                return;
+            }
+
+            if (await m_PermissionChecker.CheckPermissionAsync(victimUser, "DeathPermission") != PermissionGrantResult.Grant)
             {
                 return;
             }
@@ -46,6 +54,16 @@ namespace EvolutionPlugins.OpenDeathMessages.Events
                 instigatorUser?.Player.Transform.Position ?? victimUser.Player.Transform.Position);
 
             var location = m_UnturnedLocationDirectory.GetNearestLocation(victimPosition);
+
+            var messageSpanish = m_StringLocalizer[$"deathCauseSpanish:{@event.DeathCause.ToString().ToLower()}", new
+            {
+                Victim = victimUser,
+                Instigator = instigatorUser,
+                @event.DeathPosition,
+                Distance = distance,
+                Node = location?.Name ?? string.Empty,
+                Limb = m_StringLocalizer[$"limbParseSpanish:{@event.Limb.ToString().ToLower()}"].Value
+            }];
 
             var message = m_StringLocalizer[$"deathCause:{@event.DeathCause.ToString().ToLower()}", new
             {
@@ -63,7 +81,7 @@ namespace EvolutionPlugins.OpenDeathMessages.Events
             }
 
             await m_PlayerMessager.SendMessageGlobalOrGroupAsync(victimUser.Player, message,
-                m_Configuration["iconUrl"], ColorTranslator.FromHtml(m_Configuration["color"]));
+              m_Configuration["iconUrl"], ColorTranslator.FromHtml(m_Configuration["color"]));
         }
     }
 }
